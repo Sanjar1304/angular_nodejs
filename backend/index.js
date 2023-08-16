558cb79ab1d8e3e15606c59bcdf4eb6f22d6b907
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-08-16 16:31:25 +0500

    database connection is done

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
+const mysql = require('mysql2');
 
 
 const app = express();
@@ -8,4 +9,23 @@ const app = express();
 app.use(cors());
 app.use(bodyparser.json());
 
-app.listen(3000)
+
+// database connection
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'sanjar1304',
+    database: 'simpledb',
+    port: 3306
+})
+
+// check database connection
+db.connect((err) => {
+    return !err ? console.log('database connected') : console.log(err);
+})
+
+
+
+app.listen(3000, () => {
+    console.log('server running...!')
+})
