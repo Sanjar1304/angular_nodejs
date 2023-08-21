1932b17a076276b308f5538a5255a69fcde67199
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-08-21 17:38:55 +0500

    Added getAllData and getByIDData methods

--- frontend/src/app/api-service.service.ts
+++ frontend/src/app/api-service.service.ts
@@ -8,5 +8,23 @@ export class ApiServiceService {
 
   constructor(private http: HttpClient) {}
 
-  
+
+  // connect frontend to backend
+  apiURL = 'http://localhost:3000/'
+
+
+  // get all  data
+  getALLData(){
+    return this.http.get(`${this.apiURL}user`);
+  }
+
+
+
+  // get data by ID
+  getDataByID(id: number){
+    return this.http.get(`${this.apiURL}/user/${id}`);
+  }
+
+
+
 }
