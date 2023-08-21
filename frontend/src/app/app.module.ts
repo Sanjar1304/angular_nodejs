0e6267809e4edad2f276a3492569cfd607e4bcdb
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-08-21 17:21:54 +0500

    Added httpClientModule

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
 import { CreateComponent } from './create/create.component';
+import { HttpClientModule } from '@angular/common/http'
+import { NgModule } from '@angular/core';
 import { ReadComponent } from './read/read.component';
 
 @NgModule({
@@ -14,7 +14,8 @@ import { ReadComponent } from './read/read.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
