57c1a0a87640f5eefb046d0ad051a0c4ca951719
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-08-21 16:00:43 +0500

    Added update method by ID

--- backend/index.js
+++ backend/index.js
@@ -48,15 +48,56 @@ app.get('/user/:id', (req, res) => {
     let query = `select * from user where id = ${getID}`;
 
     db.query(query, (err, result) => {
-        if (err) console.log(err, 'error')
+        if (err) console.log(err, 'error');
+
         if (result.length > 0) {
             res.send({
-                message: 'required user data',
+                message: 'get single data',
                 data: result
+            })
+        } else {
+            res.send({
+                message: 'data not found',
             })
         }
     })
 })
+
+
+
+// post data
+app.post('/user', (req, res) => {
+    let fullname = req.body.fullname;
+    let email = req.body.email;
+    let mobile = req.body.mobile;
+    let query = `insert into user(fullname,email,mobile) values('${fullname}','${email}','${mobile}')`
+    db.query(query, (err, result) => {
+        if (err) console.log(err);
+        res.send({
+            message: 'data inserted',
+        })
+    })
+})
+
+
+
+// update single data
+app.put('/user/:id', (req, res) => {
+
+    let id = req.params.id;
+    let fullname = req.body.fullname;
+    let email = req.body.email;
+    let mobile = req.body.mobile;
+
+    let query = `update user set fullname = '${fullname}', email = '${email}', mobile = '${mobile}' where id = ${id}`;
+    db.query(query, (err, result) => {
+        if (err) console.log(err);
+        res.send({
+            message: 'data updated',
+        })
+    })
+})
+
 
 
 
