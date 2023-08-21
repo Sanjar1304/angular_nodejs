eb85ce2366e586588eadb002f6bc253e5343a849
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-08-21 17:18:25 +0500

    Added routes and and started router-outlet

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { RouterModule, Routes } from '@angular/router';
 
-import { NgModule } from '@angular/core';
+import { CreateComponent } from './create/create.component';
+import { NgModule, } from '@angular/core';
+import { ReadComponent } from './read/read.component';
 
 const routes: Routes = [
-  {}
+  {path: 'create', component: CreateComponent},
+  {path: 'read', component: ReadComponent}
 ];
 
 @NgModule({
